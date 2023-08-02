19c78cf4d397bcdda35cd5b00f7382e050d748b4
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-01 22:03:57 -0700

    converting to react

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 window.onload = () => {
     const rootElement = document.getElementById('root');
-    const button = document.createElement('button');
-    button.innerHTML = "Click me for current date";
-    button.addEventListener("click", () => {
-        button.innerHTML = new Date().toString();
-    });
-    rootElement.appendChild(button);
+    const root = ReactDOM.createRoot(rootElement);
+
+    const ints = [1,2,3];
+    
+    const childrenElements = [];
+
+    childrenElements.push(
+        React.createElement('li', {key: ints[0]}, ints[0])
+    )
+
+    childrenElements.push(
+        React.createElement('li', {key: ints[1]}, ints[1])
+    )
+
+    childrenElements.push(
+        React.createElement('li', {key: ints[2]}, ints[2])
+    )
+
+    root.render(childrenElements);
 }
